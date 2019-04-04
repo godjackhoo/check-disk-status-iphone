421f650369e1d87e1ffde6080b9334164728d806
Jack HaXOr <godjackhoo@gmail.com>
2019-04-04 15:20:29 +0700

    Add files via upload

---
+++ DiskStatus/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  DiskStatus
+//
+//  Created by Cuong Lam on 3/29/15.
+//  Copyright (c) 2015 BE Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var totalView: UIView!
+    @IBOutlet var usedLabel: UILabel!
+    @IBOutlet var freeLabel: UILabel!
+    
+    var diskUsedView: UIView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.updateDiskStatus()
+    }
+    
+    // MARK: update disk space status
+    func updateDiskStatus() {
+        usedLabel.text = String(format:NSLocalizedString("Used %@", comment: ""), DiskStatus.usedDiskSpace)
+        freeLabel.text = String(format:NSLocalizedString("Free %@", comment: ""), DiskStatus.freeDiskSpace)
+        
+        
+        // Jack add testing
+        if DiskStatus.usedDiskSpace > DiskStatus.freeDiskSpace {
+            print("Not enough space")
+        }
+        else {
+            print("Enough space to using this")
+        }
+        
+        let width = CGFloat(Double(DiskStatus.usedDiskSpaceInBytes)/Double(DiskStatus.totalDiskSpaceInBytes)) * self.totalView.bounds.width
+        
+        diskUsedView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.totalView.bounds.height))
+        diskUsedView!.backgroundColor = self.usedLabel.textColor
+        self.totalView.addSubview(diskUsedView!)
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Swift Disk Status
+Get available/free disk space on the iPhone/iPad device using Swift.
+
+https://github.com/godjackhoo/check-disk-status-iphone/
+
+Build Requirements: Xcode 6 or later
+
+Runtime Requirements: iOS 7.0 or later
